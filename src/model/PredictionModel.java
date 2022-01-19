abfce8da5ed38320d2f9daf6e50cff81c251be20
djoiiwalraven <dd.walraven@gmail.com>
2022-01-19 11:28:23 +0100

    model organised

--- src/model/PredictionModel.java
+++ src/model/PredictionModel.java
@@ -15,33 +15,26 @@ public class PredictionModel {
 	private List<Double> beta; //Weights finalized
 	
 	public PredictionModel(String fileName){
-		this.fileName = fileName + ".csv";
-		beta = new ArrayList<>();
+		this.fileName = path+"/"+fileName + ".csv";
+		this.beta = new ArrayList<>();
 		CSVRecord temp = ReadCSV.returnAsList(this.fileName, ',', 0, false).get(0);
-		System.out.println(temp);
 		for(String str : temp) {
-			beta.add(Double.valueOf(str));
+			this.beta.add(Double.valueOf(str));
 		}
 	}
 	
 	public double makePrediction(List<Double> values) {
 		double prediction = 0;
-		if(beta.size() == values.size()) {
+		if(beta.size()-1 == values.size()) {
 			for(int i = 1; i < beta.size(); i++) {
-				prediction += beta.get(i)*values.get(i-1);
+				prediction += (double)beta.get(i)*values.get(i-1);
 			}
-			prediction += beta.get(0); 
+			prediction += (double)beta.get(0);
+			System.out.println(prediction);
 		}else {
 			System.out.println("Input arrays need to have same lengths! prediction=0");
 		}
 		return prediction;
 	}
-	
-	public double error(double realAnswer, double prediction) {
-		return realAnswer - prediction;
-	}
-	
-	
-	
 	
 }
--- src/utils/GetParams.java
+++ src/utils/GetParams.java
@@ -2,11 +2,13 @@ package utils;
 
 import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
 
-public class GetWeights {
+public class GetParams {
 	
-public static void writeCSV(double[] y, double[][] x, String path, String fileName) {
+public static void writeParamsCSV(double[] y, double[][] x, String path, String fileName) {
 		OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
 		regression.newSampleData(y, x);
+		System.out.println(regression.calculateRSquared());
+		System.out.println(regression.calculateAdjustedRSquared());
 		double[] beta = regression.estimateRegressionParameters();
 		WriteCSV.write(path, fileName, beta );
 	}
